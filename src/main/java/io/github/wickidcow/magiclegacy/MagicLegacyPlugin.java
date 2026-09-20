df2798081007e2f80325c3a5b477a333e8a21a43
wickidcow <jaywago@gmail.com>
2026-09-19 22:45:25 -0400

    Add src/main/java/io/github/wickidcow/magiclegacy/MagicLegacyPlugin.java diagnostics and lifecycle

---
+++ src/main/java/io/github/wickidcow/magiclegacy/MagicLegacyPlugin.java
@@ -0,0 +1,56 @@
+package io.github.wickidcow.magiclegacy;
+
+import java.io.File;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MagicLegacyPlugin extends JavaPlugin {
+
+    private PackDeploymentResult deploymentResult;
+    private MagicLegacyDiagnostics diagnostics;
+
+    @Override
+    public void onLoad() {
+        deploymentResult = new EmbeddedPackDeployer(this).deploy();
+    }
+
+    @Override
+    public void onEnable() {
+        diagnostics = new MagicLegacyDiagnostics(this);
+
+        PluginCommand command = getCommand("magiclegacy");
+        if (command != null) {
+            MagicLegacyCommand handler = new MagicLegacyCommand(this);
+            command.setExecutor(handler);
+            command.setTabCompleter(handler);
+        }
+
+        getServer().getScheduler().runTask(this, () -> {
+            diagnostics.logStartupReport();
+            if (getServer().getPluginManager().getPlugin("RykenSlimefunCustomizer") == null) {
+                getLogger().warning(
+                    "RykenSlimefunCustomizer is not installed. Magic Legacy 2.0 currently uses "
+                        + "the managed RSC runtime while scripted systems are migrated to native Java."
+                );
+            }
+        });
+    }
+
+    File pluginFile() {
+        return getFile();
+    }
+
+    PackDeploymentResult deploymentResult() {
+        return deploymentResult;
+    }
+
+    MagicLegacyDiagnostics diagnostics() {
+        return diagnostics;
+    }
+
+    PackDeploymentResult redeploy() {
+        deploymentResult = new EmbeddedPackDeployer(this).deploy();
+        diagnostics = new MagicLegacyDiagnostics(this);
+        return deploymentResult;
+    }
+}
