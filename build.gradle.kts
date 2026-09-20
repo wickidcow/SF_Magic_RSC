5389bb0d0aa068482090c1fc6253410460a93fd7
wickidcow <jaywago@gmail.com>
2026-09-20 16:25:35 -0400

    release: bump Magic Legacy to 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 }
 
 group = "io.github.wickidcow"
-version = "2.0.20"
+version = "2.0.21"
 description = "Magic Legacy managed runtime bridge and migration host for Slimefun Legacy"
 
 java {
