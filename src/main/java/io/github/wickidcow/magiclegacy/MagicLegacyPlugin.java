61a6c3589a23e90b75941fd3ed4eaabda77def61
wickidcow <jaywago@gmail.com>
2026-09-21 14:25:10 -0400

    Register native Magic global listeners

--- src/main/java/io/github/wickidcow/magiclegacy/MagicLegacyPlugin.java
+++ src/main/java/io/github/wickidcow/magiclegacy/MagicLegacyPlugin.java
@@ -47,8 +47,7 @@ public final class MagicLegacyPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new MagicDustPickaxeListener(), this);
         getServer().getPluginManager().registerEvents(new MagicArchaeologyBlockListener(), this);
 
-        getServer().getPluginManager().registerEvents(new StorageSkinFixListener(), this);
-        nativeSpawnerManager = new NativeSpawnerManager(this);
+        getServer().getPluginManager().registerEvents(new StorageSkinFixListener(), this);\n        getServer().getPluginManager().registerEvents(new StorageCompatibilityListener(this), this);\n        getServer().getPluginManager().registerEvents(new MagicFishingListener(), this);\n        nativeSpawnerManager = new NativeSpawnerManager(this);
         nativeSpawnerManager.start();
         nativePlainMachineManager = new NativePlainMachineManager(this);
         nativePlainMachineManager.start();
